3757a378e08c68a69881fbb4acc240456426f71a
ReaganOswald <reagan.marie.oswald@gmail.com>
2026-04-02 00:24:10 -0400

    done

--- code/main_example.py
+++ code/main_example.py
@@ -1,11 +1,6 @@
 '''Module 3: count black and white pixels and compute the percentage of white pixels in a .jpg image and extrapolate points'''
 
 
-'''
-Updated code uses less loops 
-Simplifying the process and time it takes for the code to analyze the 6 images 
-'''
-
 from termcolor import colored
 import cv2
 import numpy as np
@@ -74,14 +69,12 @@ print("CSV file 'Percent_White_Pixels.csv' created.")
 '''the .csv writing subroutine ends here'''
 
 
-#############
-### LECTURE 2: UNCOMMENT BELOW
+##############
+# LECTURE 2: UNCOMMENT BELOW
 
 # Interpolate a point: given a depth, find the corresponding white pixel percentage
-<<<<<<< HEAD
 
-interpolate_depth = float(input(colored(
-    "Enter the depth at which you want to interpolate a point (in microns): ", "yellow")))
+interpolate_depth = float(input(colored("Enter the depth at which you want to interpolate a point (in microns): ", "yellow")))
 
 x = depths
 y = white_percents
@@ -112,59 +105,9 @@ axs[1].set_title('Plot of depth of image vs percentage white pixels with interpo
 axs[1].set_xlabel('depth of image (in microns)')
 axs[1].set_ylabel('white pixels as a percentage of total pixels')
 axs[1].grid(True)
-axs[1].scatter(depths_i[len(depths_i)-1], white_percents_i[len(white_percents_i)-1],
-               color='red', s=100, label='Highlighted point')
+axs[1].scatter(depths_i[len(depths_i)-1], white_percents_i[len(white_percents_i)-1],color='red', s=100, label='Highlighted point')
 
 
 # Adjust layout to prevent overlap
 plt.tight_layout()
 plt.show(block=True)
-=======
-
-
-
-interpolate_depth = float(input(colored(
-    "Enter the depth at which you want to interpolate a point (in microns): ", "yellow")))
-
-x = depths
-y = white_percents
-
-# You can also use 'quadratic', 'cubic', etc. Tell python whatever you need
-i = interp1d(x, y, kind='')
-i = bary
-interpolate_point = i(interpolate_depth)
-print(colored(
-    f'The interpolated point is at the x-coordinate {interpolate_depth} and y-coordinate {interpolate_point}.', "green"))
-
-depths_i = depths[:]
-depths_i.append(interpolate_depth)
-white_percents_i = white_percents[:]
-white_percents_i.append(interpolate_point)
-
-
-# make two plots: one that doesn't contain the interpolated point, just the data calculated from your images, and one that also contains the interpolated point (shown in red)
-fig, axs = plt.subplots(2, 1)
-
-axs[0].scatter(depths, white_percents, marker='o', linestyle='-', color='blue')
-axs[0].set_title('Plot of depth of image vs percentage white pixels')
-axs[0].set_xlabel('depth of image (in microns)')
-axs[0].set_ylabel('white pixels as a % of total pixels')
-axs[0].grid(True)
-
-
-axs[1].scatter(depths_i, white_percents_i, marker='o',
-               linestyle='-', color='blue')
-axs[1].set_title(
-    'Plot of depth of image vs percentage white pixels with interpolated point (in red)')
-axs[1].set_xlabel('depth of image (in microns)')
-axs[1].set_ylabel('white pixels as a % of total pixels')
-axs[1].grid(True)
-axs[1].scatter(depths_i[len(depths_i)-1], white_percents_i[len(white_percents_i)-1],
-               color='red', s=100, label='Highlighted point')
-
-
-# Adjust layout to prevent overlap
-plt.tight_layout()
-plt.show()
-print('fin')
->>>>>>> origin/Daniel,-Tomas_M3
